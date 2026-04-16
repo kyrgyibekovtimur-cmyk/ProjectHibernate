847d41be75b3083f4c0548b3ee5881b4ca73e7ff
Timur <kyrgyibekovtimur@gmail.com>
2026-04-16 11:51:14 +0600

    commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,14 +15,6 @@ public class App
     public static void main( String[] args ){
         //System.out.println(HibernateConfig.getSession());
         ProductService productService = new ProductServiceImpl();
-//        productService.saveProduct(new Product("iPhone 13", 56898, 999.99, "Electronics"));
-//        productService.saveProduct(new Product("Samsung Galaxy S21", 589685, 899.99, "Electronics"));
-//        productService.saveProduct(new Product("Sony WH-1000XM4", 68685, 349.99, "Audio"));
-//        productService.saveProduct(new Product("Dell XPS 13", 34, 1199.99, "Computers"));
-//        productService.saveProduct(new Product("Apple Watch Series 6", 34, 399.99, "Wearables"));
-//
-        System.out.println(productService.getProductsByCategory("Audio"));
-
 
     }
 }
